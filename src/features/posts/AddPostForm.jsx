05b9842aef6d091dda6106e57c4436e11206fd49
Subham Das <subham0464@gmail.com>
2023-03-16 23:00:19 +0530

    Create AddPostForm.jsx

---
+++ src/features/posts/AddPostForm.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+
+import { postAdded } from "./postSlice";
+import { selectAllUsers } from "../users/usersSlice";
+
+const AddPotForm = () => {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const users = useSelector(selectAllUsers);
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setUserId(e.target.value);
+
+  const onSavePostClicked = (e) =>{
+    if(title && content){
+      dispatch(
+        postAdded(title, content, userId),
+      );
+    }
+
+    setTitle("");
+    setContent("");
+    setUserId("");
+  }
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  const userOptions = users.map(user =>{
+    return(
+      <option value={user.id} key={user.id} >
+        {user.name}
+      </option>
+    )
+    
+  })
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form >
+        <label htmlFor="postTitle">Post title</label>
+        <input 
+          type="text" 
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postAuthor">Author:</label>
+        <select name="postAuthor" id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {userOptions}
+        </select>
+        <label htmlFor="postContent">Content</label>
+        <textarea 
+          name="postContent" 
+          id="postContent"
+          value={content}
+          onChange={onContentChanged} 
+          />
+          <button 
+            onClick={onSavePostClicked} 
+            type="button" 
+            disabled={!canSave}
+          >
+            Save Post
+          </button>
+      </form>
+    </section>
+  )
+}
+
+export default AddPotForm;
