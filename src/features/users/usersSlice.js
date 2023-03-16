889bb4ec7d732a76b94c4b921cb6a09339198f0a
Subham Das <subham0464@gmail.com>
2023-03-17 00:52:20 +0530

    Update usersSlice.js

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,9 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-    {id: '0', name: 'Subham Grass'},
-    {id: '1', name: 'Amar Santosh'},
-    {id: '2', name: 'C Harshit'},
+    {id: '0', name: 'Subham'},
+    {id: '1', name: 'Jeff'},
+    {id: '2', name: 'Mike'},
+    {id: '3', name: 'Hannah'},
+    {id: '4', name: 'Amit'},
+    {id: '5', name: 'Michael'},
 ]
 
 const usersSlice= createSlice({
