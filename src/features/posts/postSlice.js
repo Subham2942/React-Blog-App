4dcb142ef1cdc1ffec065ce3f22cab2a28c54076
Subham Das <subham0464@gmail.com>
2023-03-19 02:53:00 +0530

    Update postSlice.js

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -107,5 +107,7 @@ export const selectAllPosts = state => state.posts.posts;
 export const getPostsStatus = state => state.posts.status;
 export const getPostsError = state => state.posts.error;
 
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId);
+
 export const {postAdded, reactionAdded} = postSlice.actions;
 export default postSlice.reducer;
