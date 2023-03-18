93e528c050ab9e08cc0bfaed8d9971357186c954
Subham Das <subham0464@gmail.com>
2023-03-18 14:31:46 +0530

    Update PostsList.jsx

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,26 +1,38 @@
-import { useSelector } from "react-redux";
-import { selectAllPosts } from "./postSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { selectAllPosts, getPostsError, getPostsStatus, fetchPosts } from "./postSlice";
+import { useEffect } from "react";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 
 const PostsList = () => {
-    const posts = useSelector(selectAllPosts);
+  
+  const dispatch = useDispatch();
 
-    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date));
+  const posts = useSelector(selectAllPosts);
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+  
+  useEffect(()=>{
+    if(postStatus=== 'idle'){
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
 
-    const renderedPosts = orderedPosts.map(post =>(
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0,101)}</p>
+  const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date));
 
-            <p className="postCredit">
-              <PostAuthor userId={post.userId}/>
-              <TimeAgo timestamp={post.date} />
-            </p>
-            <ReactionButtons post={post} />
-        </article>
-    ))
+  const renderedPosts = orderedPosts.map(post =>(
+      <article key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.content.substring(0,101)}</p>
+
+          <p className="postCredit">
+            <PostAuthor userId={post.userId}/>
+            <TimeAgo timestamp={post.date} />
+          </p>
+          <ReactionButtons post={post} />
+      </article>
+  ))
   return (
     <section>
         <h1>Posts</h1>
