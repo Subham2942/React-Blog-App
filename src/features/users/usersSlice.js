228580456285ce9bf8c943f2ac51baedb2646921
Subham Das <subham0464@gmail.com>
2023-03-19 00:15:24 +0530

    Update usersSlice.js

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,4 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const USERS_URL = "https://jsonplaceholder.typicode.com/users"
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () =>{
+    try{
+        const response = await axios.get(USERS_URL);
+        return response.data;
+    }
+    catch(err){
+        return err.message;
+    }
+})
+
 
 const initialState = [
     {id: '0', name: 'Subham'},
@@ -12,7 +26,12 @@ const initialState = [
 const usersSlice= createSlice({
     name: 'users',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, (state,action)=>{
+            return action.payload;
+        })
+    }
 })
 
 export const selectAllUsers = (state) => state.users;
