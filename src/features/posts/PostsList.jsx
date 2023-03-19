c446796cc44f8abad0c7a80df6765f1500bc0cde
Subham Das <subham0464@gmail.com>
2023-03-19 15:06:56 +0530

    Update PostsList.jsx

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,22 +1,14 @@
-import { useSelector, useDispatch } from "react-redux";
-import { selectAllPosts, getPostsError, getPostsStatus, fetchPosts } from "./postSlice";
-import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectAllPosts, getPostsError, getPostsStatus } from "./postSlice";
 
 import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
-  
-  const dispatch = useDispatch();
 
   const posts = useSelector(selectAllPosts);
   const postStatus = useSelector(getPostsStatus);
   const error = useSelector(getPostsError);
-  
-  useEffect(()=>{
-    if(postStatus=== 'idle'){
-      dispatch(fetchPosts());
-    }
-  }, [postStatus, dispatch]);
+
 
   let content;
   if(postStatus === "loading"){
