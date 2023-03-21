9ef2029af19f0b7032fc1db05d53e7841a49afac
Subham Das <subham0464@gmail.com>
2023-03-21 21:56:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import AddPostForm from "./features/posts/AddPostForm";
 import EditPostForm from "./features/posts/EditPostForm";
 
 import { Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import SinglePostPage from "./features/posts/SinglePostPage";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
 
 function App() {
   return (
@@ -15,8 +18,16 @@ function App() {
         <Route path="post">
           <Route  index element={<AddPostForm/>} />
           <Route path=":postId" element={<SinglePostPage/>} />
-          <Route path="edit/:postId" element={<EditPostForm/>} />
+          <Route path="edit/:postId" element={<EditPostForm/>} /> n
         </Route>
+
+        <Route path="user">
+          <Route  index element={<UsersList/>} />
+          <Route path=":userId" element={<UserPage/>} /> n
+        </Route>
+
+        {/* catch all - can be replaced with 404 component */}
+        <Route path="*"  element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
