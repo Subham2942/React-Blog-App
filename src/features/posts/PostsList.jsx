a36e623328b41e5375f6a44af52e2181a948008c
Subham Das <subham0464@gmail.com>
2023-03-22 01:09:13 +0530

    Update PostsList.jsx

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
-import { selectAllPosts, getPostsError, getPostsStatus } from "./postSlice";
+import { selectPostIds, getPostsError, getPostsStatus } from "./postSlice";
 
 import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
 
-  const posts = useSelector(selectAllPosts);
+  const orderedPostsIds = useSelector(selectPostIds);
   const postStatus = useSelector(getPostsStatus);
   const error = useSelector(getPostsError);
 
@@ -14,8 +14,7 @@ const PostsList = () => {
   if(postStatus === "loading"){
     content = <p>"Loading...</p>;
   } else if(postStatus === "succeeded"){
-    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date));
-    content = orderedPosts.map(post  => <PostsExcerpt key={post.id} post={post} />)
+    content = orderedPostsIds.map(postId => <PostsExcerpt key={postId} postId={postId}/>)
   } else if(postStatus === "failed"){
     content = <p>{error}</p>;
   }
